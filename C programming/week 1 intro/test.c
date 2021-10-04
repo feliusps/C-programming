29d3d700f2fb82356c105d96e40735b3976a52ae
Felix <27907799+feliusps@users.noreply.github.com>
2021-10-04 15:26:39 -0700

    Delete C programming directory

--- C programming/week 1 intro/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main () {
-   char yrname;
-   printf("Please enter your name:");
-   scanf("%c",&yrname);
-   printf("\n %c, do not go gentle into the good night. \n", yrname);
-   return(0);
-}
