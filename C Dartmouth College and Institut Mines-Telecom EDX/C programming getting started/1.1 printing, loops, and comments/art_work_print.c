7ab88dd6627f5fb993ffcefb3a0cca8120ba4c2a
Felix <27907799+feliusps@users.noreply.github.com>
2021-11-22 10:53:08 -0800

    Delete C Dartmouth College and Institut Mines-Telecom EDX/C programming getting started/1.1 printing, loops, and comments directory

--- C Dartmouth College and Institut Mines-Telecom EDX/C programming getting started/1.1 printing, loops, and comments/art_work_print.c
+++
@@ -1,9 +0,0 @@
-
-//display the piece of abstract art, using just a single printf command.
-
-#include <stdio.h>
-
-int main(void){
-    printf("*****\n**|**\n*|.|*\n|...|\n.....");
-    return 0;
-}
